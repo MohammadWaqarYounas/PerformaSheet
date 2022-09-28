e795b51a8452f8207f62a6845f0c537859fbc46b
MohammadWaqarYounas <waqary268@gmail.com>
2022-09-28 23:00:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/performasheet/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.performasheet;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    TextView textView;
+    DatePickerDialog.OnDateSetListener dateset;
+    Spinner spinner1,spinner2,spinner3;
+    String[] cities={"Select City","Lahore","Faisalabad","Karachi","Multan","Islamabad"};
+    ArrayAdapter adaptor;
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.datepicker);
+        spinner1 = findViewById(R.id.spinner1);
+        spinner2 = findViewById(R.id.spinner2);
+        spinner3 = findViewById(R.id.spinner3);
+        spinner1.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(this);
+        spinner3.setOnItemSelectedListener(this);
+
+
+        adaptor= new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, cities);
+        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adaptor);
+
+        adaptor= new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,cities);
+        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adaptor);
+
+        adaptor= new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,cities);
+        adaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adaptor);
+
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar=Calendar.getInstance();
+                int year=calendar.get(Calendar.YEAR);
+                int month=calendar.get(Calendar.MONTH);
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog= new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,dateset,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+                dateset = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month=month+1;
+                        String date=month+"/"+day+"/"+year;
+                        textView.setText(date);
+                    }
+                };
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
